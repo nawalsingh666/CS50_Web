209971a42503cb31a2144c50364b19387c6eb68b
Nawal Singh <nawal@identy.io>
2020-06-13 20:16:31 +0530

    In app running facility(shift+F10)

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session # does NOT comes with flask by default
-
+# from flask.ext.session import Session
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+# app.secret_key = "Top Secret !!!"
 
 global_notes = []
 
@@ -13,17 +14,19 @@ global_notes = []
 def index():
     if request.method == 'POST':
         global_notes.append(request.form.get('note'))
-    print(global_notes)
-    return render_template("global_chat.html", notes=global_notes)
+    heading = "GLOBAL NOTES"
+    return render_template("global_notes.html", notes=global_notes, heading_name=heading)
 
 
 @app.route("/private", methods = ['GET', 'POST'])
 def private():
+    # print("full", session)
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == 'POST':
-        if session.get("notes") is None:
-            session["notes"] = None;
         session["notes"].append(request.form.get("note"))
-    return render_template("global_chat.html", notes=session["notes"])
+    heading = "LOCAL NOTES"
+    return render_template("global_notes.html", notes=session["notes"], heading_name=heading)
 
 
 # Error handling, ....................................
@@ -35,3 +38,8 @@ def error_404(e):
 
 app.register_error_handler(404, error_404)
 
+
+# run it !!!
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=2010, threaded=True)
